e87f5c05d643fa294ec8414bb9419d13a6c2b25e
flzl2008 <goddlaek@naver.com>
2022-06-06 16:05:22 +0900

    [#3] 게시물 리스트 mock 데이터 생성

--- src/js/components/PostList.tsx
+++ src/js/components/PostList.tsx
@@ -1,22 +1,38 @@
 import { useState } from 'react';
 
 export default function PostList() {
-  const [postList, setPostList] = useState<Post[]>([]);
+  const mockPostList = [
+    {
+      id: 1,
+      title: 'test',
+      content: '게시글 내용입니다.',
+      category: 'notice',
+    },
+    {
+      id: 2,
+      title: 'test2',
+      content: '게시글 내용입니다2.',
+      category: 'info',
+    },
+  ];
+  const [postList, setPostList] = useState<Post[]>(mockPostList);
 
   return (
     <>
       <h1>게시글 목록</h1>
-      {postList.map(post => {
-        <article className="post-list">
-          <div className="post-list__title">
-            <span>{post.category}</span>
-            <span>{post.title}</span>
-          </div>
-          <div className="post-list__content">
-            <span>{post.content}</span>
-          </div>
-        </article>;
-      })}
+      <div className="post-list">
+        {postList.map(post => (
+          <article className="post-list__item" key={post.id}>
+            <div className="post-list__item--title">
+              <span>{post.category}</span>
+              <span>{post.title}</span>
+            </div>
+            <div className="post-list__item--content">
+              <span>{post.content}</span>
+            </div>
+          </article>
+        ))}
+      </div>
     </>
   );
 }
