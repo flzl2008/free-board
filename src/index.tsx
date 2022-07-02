f342b5b43f74861ab4cdd035306706164117a9ab
flzl2008 <goddlaek@naver.com>
2022-07-02 20:41:11 +0900

    [#4] chakra ui 설치

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './js/App';
+import { ChakraProvider } from '@chakra-ui/react';
 import reportWebVitals from './js/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
